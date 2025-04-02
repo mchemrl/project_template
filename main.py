2273d8fa38f7157b43f9417526d604ec09eb9fcd
mchemrl <mcchemro@gmail.com>
2025-04-02 15:24:56 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from app.io.input import input_from_console, input_from_file, input_from_file_pandas
+from app.io.output import output_to_console, output_to_file, output_to_file_pandas
 
 def main():
-    pass
+    text_console = input_from_console()
+    output_to_console(text_console)
+    output_to_file("console_output.txt", text_console)
+
+    text_file = input_from_file("input.txt")
+    output_to_console(text_file)
+    output_to_file("file_output.txt", text_file)
+
+    df = input_from_file_pandas("data.csv")
+    output_to_console(df.to_string())
+    output_to_file_pandas("data_output.csv", df)
 
 if __name__ == '__main__':
     main()

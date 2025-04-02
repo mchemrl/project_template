1d2e3582b117171583e70df6b1d87c13ad74fa31
mchemrl <mcchemro@gmail.com>
2025-04-02 15:23:13 +0300

    added io methods

--- app/io/input.py
+++ app/io/input.py
@@ -1,8 +1,51 @@
+import pandas as pd
+
 def input_from_console():
-    pass
+    """
+    Reads input from console.
 
-def input_from_file():
-    pass
+    Returns:
+        str: the input from console.
+    """
+    return input('enter text:')
 
-def input_from_file_pandas():
-    pass
+def input_from_file(filepath):
+    """
+    Reads text from file using Python built-in methods.
+
+    Args:
+        filepath (str): path to the file
+
+    Returns:
+        str: the input from file.
+
+    Raises:
+        FileNotFoundError: if the file does not exist.
+    """
+    try:
+        with open(filepath, 'r') as f:
+            return f.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f'file {filepath} not found')
+
+def input_from_file_pandas(filename):
+    """
+    Reads input in csv from Pandas DataFrame.
+
+    Args:
+        filepath (str): path to the file
+
+    Returns:
+        pandas.DataFrame: the input from Pandas DataFrame.
+
+    Raises:
+        FileNotFoundError: if the file does not exist.
+        IOError: if error occured when reading the file.
+    """
+
+    try:
+        return pd.read_csv(filename)
+    except FileNotFoundError:
+        raise FileNotFoundError(f'file {filename} not found')
+    except IOError:
+        raise IOError(f'file {filename} not read')
--- app/io/output.py
+++ app/io/output.py
@@ -1,8 +1,43 @@
-def output_from_console():
-    pass
+import pandas as pd
 
-def output_from_file():
-    pass
+def output_to_console(text):
+    """
+    Outputs text to console.
 
-def output_from_file_pandas():
-    pass
+    Args:
+        text (str): the text to print to console.
+    """
+    print(text)
+
+def output_to_file(filepath, text):
+    """
+    Writes text to a file using Python built-in methods.
+
+    Args:
+        filepath (str): path to the file.
+        text (str): the text to write to the file.
+
+    Raises:
+        IOError: if an error occurs when writing to the file.
+    """
+    try:
+        with open(filepath, 'w') as f:
+            f.write(text)
+    except IOError:
+        raise IOError(f'error writing to file {filepath}')
+
+def output_to_file_pandas(filepath, dataframe):
+    """
+    Writes a Pandas DataFrame to a CSV file.
+
+    Args:
+        filepath (str): path to the output CSV file.
+        pandas.DataFrame: the DataFrame to save.
+
+    Raises:
+        IOError: if an error occurs when writing the file.
+    """
+    try:
+        dataframe.to_csv(filepath, index=False)
+    except IOError:
+        raise IOError(f'error writing to file {filepath}')
